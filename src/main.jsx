cd293a0daad33bdd7733f0eee1eee5d67c16b099
elvionweb <elvionweb@gmail.com>
2026-04-24 16:54:29 +0200

    Updating the music on the live link

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Howler } from "howler";
 import App from "./App";
 import "./index.css";
+
+// Increase Howler's HTML5 Audio pool size to prevent exhaustion errors
+// especially caused by hot-restarts or React StrictMode creating instances 
+// that can't be cleanly unloaded while locked.
+Howler.html5PoolSize = 100;
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
